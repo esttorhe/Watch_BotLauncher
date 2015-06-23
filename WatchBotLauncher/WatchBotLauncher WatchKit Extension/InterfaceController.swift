2fd84865776b197e9c8b222329414bfd32addc86
esttorhe <me@estebantorr.es>
2015-06-23 15:25:29 -0600

    Created placeholder project.

---
+++ WatchBotLauncher/WatchBotLauncher WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchBotLauncher WatchKit Extension
+//
+//  Created by Esteban Torres on 23/6/15.
+//  Copyright © 2015 Esteban Torres. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
